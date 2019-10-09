d6b17b655f26e9bbaf50990ae3b4ba92bbdc0738
Shola Ayeni <ayenisholah@yahoo.com>
2019-10-09 14:30:52 +0100

    get current min and max

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -1,15 +1,32 @@
 #!/usr/bin/python
-
 import argparse
 
+
 def find_max_profit(prices):
-  pass
+	# Get current min and max
+	current_min = 0
+	current_max = 1
+	for i in range(0, len(prices)-1):
+		if prices[i] < current_min:
+			current_min = prices[i]
+		elif prices[i] > current_max:
+			current_max = prices[i]
+    
+  # Check if the index of max > min
+  # if index of max > min: subtract numbers and return result
+  # else: return
+	pass
 
 
 if __name__ == '__main__':
   # This is just some code to accept inputs from the command line
-  parser = argparse.ArgumentParser(description='Find max profit from prices.')
-  parser.add_argument('integers', metavar='N', type=int, nargs='+', help='an integer price')
-  args = parser.parse_args()
+    parser = argparse.ArgumentParser(
+        description='Find max profit from prices.')
+    parser.add_argument('integers', metavar='N', type=int,
+                        nargs='+', help='an integer price')
+    args = parser.parse_args()
 
-  print("A profit of ${profit} can be made from the stock prices {prices}.".format(profit=find_max_profit(args.integers), prices=args.integers))
+    profit = find_max_profit(args.integers)
+
+    print("A profit of ${profit} can be made from the stock prices {prices}.".format(
+        profit=find_max_profit(args.integers), prices=args.integers))
